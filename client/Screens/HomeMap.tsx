acef742b0261e1e6afd63d8e698ac18500d76cd2
mohammedaitgaba <93977101+mohammedaitgaba@users.noreply.github.com>
2023-03-13 13:58:38 +0100

    add modal that cotaine map to make the user choose the place of the company

--- client/Screens/HomeMap.tsx
+++ client/Screens/HomeMap.tsx
@@ -1,43 +1,44 @@
-import {View, StyleSheet,Button} from 'react-native';
-import React, { useEffect,useState } from 'react';
+import {View, StyleSheet, Button} from 'react-native';
+import React, {useEffect} from 'react';
 import MapboxGL from '@rnmapbox/maps';
 import Geolocation from 'react-native-geolocation-service';
-import { PermissionsAndroid } from 'react-native';
+import {PermissionsAndroid} from 'react-native';
 
 MapboxGL.setWellKnownTileServer('Mapbox');
 MapboxGL.setAccessToken(
   'pk.eyJ1IjoibWVkb3gtcyIsImEiOiJjbGV5NGo3Z2cwOWQxM3lvNTRwOXh0N2NlIn0.0LJp5QXaKFxIhb4gMqmXag',
 );
-const HomeMap = ({navigation}:any) => {
+const HomeMap = ({navigation}: any) => {
   const initialCamera = {
     centerCoordinate: [-9.2353665, 32.2935008], // San Francisco
     zoomLevel: 4,
   };
 
   useEffect(() => {
-      requestLocationPermission()
-  })
-  
-  const getUserPosition =()=>{
+    requestLocationPermission();
+  });
+
+  const getUserPosition = () => {
     Geolocation.getCurrentPosition(
       position => {
-        const { latitude, longitude } = position.coords;
+        const {latitude, longitude} = position.coords;
         console.log('Latitude:', latitude);
         console.log('Longitude:', longitude);
       },
       error => {
         console.log(error);
       },
-      { enableHighAccuracy: true},
+      {enableHighAccuracy: true},
     );
-  }
+  };
   async function requestLocationPermission() {
     try {
       const granted = await PermissionsAndroid.request(
         PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
         {
           title: 'Location Permission',
-          message: 'This app needs access to your location to show your current location on the map.',
+          message:
+            'This app needs access to your location to show your current location on the map.',
           buttonNeutral: 'Ask Me Later',
           buttonNegative: 'Cancel',
           buttonPositive: 'OK',
@@ -45,7 +46,7 @@ const HomeMap = ({navigation}:any) => {
       );
       if (granted === PermissionsAndroid.RESULTS.GRANTED) {
         console.log('Location permission granted.');
-          getUserPosition()
+        getUserPosition();
       } else {
         console.log('Location permission denied.');
       }
@@ -53,20 +54,19 @@ const HomeMap = ({navigation}:any) => {
       console.warn(err);
     }
   }
-  const handleMapLongPress = (event:any)=>{
-    console.log(event.geometry.coordinates)
-  }
+  const handleMapLongPress = (event: any) => {
+    console.log(event.geometry.coordinates);
+  };
   return (
     <View>
       <View style={styles.container}>
-     <MapboxGL.MapView style={styles.map} onLongPress={handleMapLongPress}>
-        <MapboxGL.Camera {...initialCamera} />
-    </MapboxGL.MapView>
-
+        <MapboxGL.MapView style={styles.map} onLongPress={handleMapLongPress}>
+          <MapboxGL.Camera {...initialCamera} />
+        </MapboxGL.MapView>
       </View>
       <View style={styles.buttonHolder}>
         <Button
-          onPress={()=>navigation.navigate('Login', {name: 'Jane'})}
+          onPress={() => navigation.navigate('Login', {name: 'Jane'})}
           title="Learn More"
         />
       </View>
@@ -77,23 +77,23 @@ const HomeMap = ({navigation}:any) => {
 export default HomeMap;
 
 const styles = StyleSheet.create({
-    page: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    container: {
-      height: '90%',
-      width: '100%',
-    },
-    map: {
-      flex: 1,
-    },
-    buttonHolder:{
-      display:'flex',
-      flexDirection:'column',
-      width:'50%',
-      alignSelf:'center',
-      paddingTop:6
-    }
-  });
+  page: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  container: {
+    height: '90%',
+    width: '100%',
+  },
+  map: {
+    flex: 1,
+  },
+  buttonHolder: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '50%',
+    alignSelf: 'center',
+    paddingTop: 6,
+  },
+});
