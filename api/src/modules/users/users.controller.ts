8667783bdb95926038e8ce1232332763ba8669e9
mohammedaitgaba <93977101+mohammedaitgaba@users.noreply.github.com>
2023-03-15 16:28:29 +0100

    add user module

--- api/src/modules/users/users.controller.ts
+++ api/src/modules/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { Get } from '@nestjs/common/decorators';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private UsersService : UsersService){}
+    @Get('')
+    FetchUsers(){
+        return this.UsersService.FetchUsers()
+    }
+}
